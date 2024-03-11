309c85232c7f80fd788e91d4d82636423192ffaf
achraf abaab <firasbenkraiem8@gmail.com>
2024-03-11 12:53:09 +0100

    the is test

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/users/create")
+    public String createUser(@RequestBody User  user) {
+        userRepository.save(user);
+        return "redirect:/confirmation";
+    }
+}
+
